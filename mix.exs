c43d621b51878e28f5ab9ce083ef6f2c8685f3b5
Victor Peres <victor.peres93@gmail.com>
2018-02-16 16:35:23 -0200

    chore: initial lint config with credo

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule FinancialSystem.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:excoveralls, "~> 0.8", only: :test}
+      {:excoveralls, "~> 0.8", only: :test},
+      {:credo, "~> 0.8.10"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
