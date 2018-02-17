77104ff6535320d2a9b6a8bef462a8a41fba15ff
Victor Peres <victor.peres93@gmail.com>
2018-02-17 17:30:07 -0200

    feat: adition and subtraction in money structure

--- lib/money.ex
+++ lib/money.ex
@@ -6,13 +6,14 @@ defmodule FinancialSystem.Money do
   defstruct integer_part: nil, fractional_part: nil, currency: nil
 
   alias FinancialSystem.Currency
+  alias FinancialSystem.Money
 
   def new(amount, %Currency{} = currency) do
     if not valid_amount?(amount) do
       raise "Invalid amount format"
     end
     {:ok, 
-      %FinancialSystem.Money{
+      %Money{
         integer_part: get_integer_part(amount),
         fractional_part: get_fractional_part(amount, currency.decimal_places),
         currency: currency
@@ -30,8 +31,7 @@ defmodule FinancialSystem.Money do
     str_fractional = String.trim(str_fractional, ".")
     exponent = decimal_places - String.length(str_fractional)
     exponent = max(0, exponent)
-    fractional_part =
-      trunc(String.to_integer(str_fractional) * :math.pow(10, exponent))
+    trunc(String.to_integer(str_fractional) * :math.pow(10, exponent))
   end
 
   def valid_amount?(amount) when is_binary(amount) do
@@ -41,4 +41,50 @@ defmodule FinancialSystem.Money do
   end
 
   def valid_amount?(amount) when is_number(amount), do: false
+
+  def add(%Money{} = money1, value) when is_binary(value) do
+    {:ok, money2} = Money.new(value, money1.currency)
+    add(money1, money2)
+  end
+
+  def add(%Money{} = money1, %Money{} = money2) do
+    if money1.currency != money2.currency, do: raise "Can't add different currencies"
+
+    total = get_value_in_minor_units(money1) + get_value_in_minor_units(money2)
+
+    %Money{
+      integer_part: div(total, get_power(money1)),
+      fractional_part: rem(total, get_power(money1)),
+      currency: money1.currency
+    }
+  end
+
+  def subtract(%Money{} = money1, value) when is_binary(value) do
+    {:ok, money2} = Money.new(value, money1.currency)
+    subtract(money1, money2)
+  end
+
+
+  @doc """
+    Subtract value of second money from the first money.
+  """
+  def subtract(%Money{} = money1, %Money{} = money2) do
+    if money1.currency != money2.currency, do: raise "Can't subtract different currencies"
+
+    total = get_value_in_minor_units(money1) - get_value_in_minor_units(money2)
+
+    %Money{
+      integer_part: div(total, get_power(money1)),
+      fractional_part: rem(total, get_power(money1)),
+      currency: money1.currency
+    }
+  end
+
+  defp get_value_in_minor_units(%Money{} = money) do
+    money.integer_part * get_power(money) + money.fractional_part
+  end
+
+  defp get_power(%Money{} = money) do
+    trunc(:math.pow(10, money.currency.decimal_places))
+  end
 end
--- test/money_test.exs
+++ test/money_test.exs
@@ -6,9 +6,16 @@ defmodule MoneyTest do
   alias FinancialSystem.Currency
 
   setup_all do
+    currency_brl = %Currency{alphabetic_code: "BRL", numeric_code: 100, decimal_places: 2}
+    currency_usd = %Currency{alphabetic_code: "USD", numeric_code: 101, decimal_places: 2}
     {
       :ok,
-       currency1: %Currency{alphabetic_code: "BRL", numeric_code: 100, decimal_places: 3}
+      [
+       currency_brl: currency_brl,
+       money10_brl: %Money{integer_part: 10, fractional_part: 0, currency: currency_brl},
+       money1050_brl: %Money{integer_part: 10, fractional_part: 50, currency: currency_brl},
+       money10_usd: %Money{integer_part: 10, fractional_part: 0, currency: currency_usd}
+      ]
     }
   end
 
@@ -53,13 +60,43 @@ defmodule MoneyTest do
   end
 
   test "check new money is successfully created", state do
-    {ok, _money} = Money.new("10.5", state[:currency1])
+    {ok, _money} = Money.new("10.5", state[:currency_brl])
     assert ok == :ok
   end
 
   test "check new money with invalid amount", state do
     assert_raise RuntimeError, "Invalid amount format", fn -> 
-      Money.new("10 5", state[:currency1])
+      Money.new("10 5", state[:currency_brl])
+    end
+  end
+
+  test "add two moneys 10 + 10.5 = 20.5", state do
+    assert %Money{integer_part: 20, fractional_part: 50, currency: state[:currency_brl]} == Money.add(state[:money10_brl], state[:money1050_brl])
+  end
+
+
+  test "add two moneys 10 + 10.5 = 20.5 by value", state do
+    assert %Money{integer_part: 20, fractional_part: 50, currency: state[:currency_brl]} == Money.add(state[:money10_brl], "10.5")
+  end
+
+  test "add money with different currencies fails", state do
+    assert_raise RuntimeError, "Can't add different currencies", fn ->
+      Money.add(state[:money10_brl], state[:money10_usd])
+    end
+  end
+
+  test "subtract two moneys 10.5 - 10 = 0.5", state do
+    assert %Money{integer_part: 0, fractional_part: 50, currency: state[:currency_brl]} == Money.subtract(state[:money1050_brl], state[:money10_brl])
+  end
+
+
+  test "subtract two moneys 10.5 + 10 = 0.5 by value", state do
+    assert %Money{integer_part: 0, fractional_part: 50, currency: state[:currency_brl]} == Money.subtract(state[:money1050_brl], "10.0")
+  end
+
+  test "subtract money with different currencies fails", state do
+    assert_raise RuntimeError, "Can't subtract different currencies", fn ->
+      Money.subtract(state[:money10_brl], state[:money10_usd])
     end
   end
 end
